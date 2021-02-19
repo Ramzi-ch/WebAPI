6af6f9f654a36f6e7606cbae42858010f17da89b
RAMZI CHAKROUN <ramzi.chakroun@zeta-box.com>
2021-02-19 14:53:39 +0100

    Configure Swagger

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -11,7 +11,9 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
+using WebAPI.Swagger;
 
 namespace WebAPI
 {
@@ -44,11 +46,17 @@ namespace WebAPI
                 options.SerializerSettings.ContractResolver = new DefaultContractResolver());
 
             services.AddControllers();
+
+            //Configure Swagger launch page
+            services.AddSwaggerGen(x => { x.SwaggerDoc("v1",new OpenApiInfo{Title = "WebAPI",Version = "v1"});});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Configure Swagger
+            ConfigSwagger(app);
+
             app.UseCors(options => options.AllowAnyOrigin()
                 .AllowAnyMethod().AllowAnyHeader());
             if (env.IsDevelopment())
@@ -72,6 +80,14 @@ namespace WebAPI
                     Path.Combine(Directory.GetCurrentDirectory(),"Photos")),
                 RequestPath = "/Photos"
             });
+        }
+        //This method is used to Configure Swagger
+        private void ConfigSwagger(IApplicationBuilder app)
+        {
+            var swaggerOptions = new SwaggerOptions();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+            app.UseSwagger(option => { option.RouteTemplate = swaggerOptions.JsonRoute; });
+            app.UseSwaggerUI(option => { option.SwaggerEndpoint(swaggerOptions.UIEndpoint, swaggerOptions.Description); });
         }
     }
 }
